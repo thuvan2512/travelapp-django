f36bdcd35f5c5a9238fb3cacb1252daa2cb06251
thuvan2512 <thunguyenvan2512@gmail.com>
2022-05-21 11:27:04 +0700

    fix return id, auth_provider,..etc for userserializer

--- travelproject/travelproject/settings.py
+++ travelproject/travelproject/settings.py
@@ -196,7 +196,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'travel.agency.ou@gmail.com'
+EMAIL_HOST_USER = 'travel.agency.ou.application@gmail.com'
 EMAIL_HOST_PASSWORD = '25122000Thu@'
 EMAIL_PORT = 587
 
